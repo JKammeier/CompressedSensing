06663350deddea0293880239c9c206f35f8d327c
Jannis Kammeier <95929740+JKammeier@users.noreply.github.com>
2026-02-09 23:44:29 +0100

    improved representation of results

--- printNorms.m
+++ printNorms.m
@@ -1,10 +1,10 @@
 function [l0,l1,l2] = printNorms(vector)
-%Prints the L1 and L2 norms, as well as L0 of a vector.
-%   Here, L0 is the ration of nonzero entries to the length of the vector.
+% Prints the L1 and L2 norms, as well as L0 of a vector.
+%   Here, L0 is the ratio of nonzero entries to the length of the vector.
 
-    l0 = nnz(vector)/length(vector);
+    l0 = nnz(vector) / numel(vector);
     l1 = norm(vector,1);
     l2 = norm(vector,2);
 
-    fprintf("l0: %d\nl1: %d\nl2: %d\n\n", l0, l1, l2);
+    fprintf("l0: %f\nl1: %f\nl2: %f\n\n", l0, l1, l2);
 end
--- showImages.m
+++ showImages.m
@@ -1,16 +1,26 @@
 % C is only shown if it is nonzero
-function showImages(original, retransformed, C)
+function showImages(original, retransformed, C, s)
     figure
     
     subplot(1,2,1);
     imshow(original);
-    title("Original image");
+    title("Original image, ground truth");
     subplot(1,2,2);
     imshow(retransformed);
-    title("Retransformed image");
+    title("Retransformed image, CS example");
     
-    if (C ~= 0)
+    if (nnz(C) ~= 0)
         figure
-        imshow(C)
+        imshow(C);
+        title("C Matrix");
+    end
+    if (nnz(s) ~= 0)
+        figure
+        subplot(1,2,1);
+        histogram(s,-50:0.001:50);
+        title("Histogram of s (only showing area -50:50)");
+        subplot(1,2,2);
+        plot(s);
+        title("Values of s");
     end
 end
